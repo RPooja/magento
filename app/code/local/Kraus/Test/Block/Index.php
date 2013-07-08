deca4b5a7b7444fd6512c99fced9b096fc0dd31e
pooja <pverma@kraususa.com>
2013-07-08 23:31:16 +0530

    Registration form saving data in database
    used post method to get data

--- app/code/local/Kraus/Test/Block/Index.php
+++ app/code/local/Kraus/Test/Block/Index.php
@@ -1,5 +1,6 @@
 <?php   
-class Kraus_Test_Block_Index extends Mage_Core_Block_Template{   
+class Kraus_Test_Block_Index extends Mage_Core_Block_Template
+{   
 
 
 
--- app/code/local/Kraus/Test/controllers/IndexController.php
+++ app/code/local/Kraus/Test/controllers/IndexController.php
@@ -21,8 +21,24 @@ class Kraus_Test_IndexController extends Mage_Core_Controller_Front_Action{
 	  
     }
 	
-	public function postAction() {
-	
-	die("We are at post action");
+	       public function postAction() {
+				$model = Mage::getModel('test/test');
+			/*
+			$collection=				$model->getCollection();
+			foreach ($collection as $item ) {
+			print_r($item->getData());
+			}*/
+				$name=$this->getRequest()->getParam('name'); // HEre i'm getting value from the Post 
+				$model->setName(trim($name)); // here i saving it into the model
+				//each value that you want get from the form you have to get first of all form post and after that set to model
+				$email=$this->getRequest()->getParam('email');
+				$model->setEmail(trim($email));
+				$phone=$this->getRequest()->getParam('phone');
+				$model->setTelephone($phone);
+				$comment=$this->getRequest()->getParam('comment');
+				$model->setComments(trim($comment));
+				
+				$model->save();		
+                die("We are at post action");
 	}
 }
