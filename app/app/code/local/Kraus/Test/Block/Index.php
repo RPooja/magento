6a2cd02bdc6db7a1033946ce9f545edb13fe87a7
pooja <pverma@kraususa.com>
2013-07-08 21:31:47 +0530

    Registeration form new

---
+++ app/app/code/local/Kraus/Test/Block/Index.php
@@ -0,0 +1,8 @@
+<?php   
+class Kraus_Test_Block_Index extends Mage_Core_Block_Template{   
+
+
+
+
+
+}
--- app/code/local/Kraus/Test/Model/test.php
+++
@@ -1,9 +0,0 @@
- <?php
-
-class Kraus_Test_Model_Test extends Mage_Core_Model_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init('test/test');
-    }	
-}
